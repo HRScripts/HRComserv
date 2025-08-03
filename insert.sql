db874d77342ee1f517491fee02f8f04faf86d919
HristiyanR <85462760+HR0110@users.noreply.github.com>
2025-08-03 11:10:21 +0300

    fix(insert.sql): removing unecessary code that also might cause problems

--- insert.sql
+++ insert.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS `community_services`;
 CREATE TABLE `community_services` (
     `identifier` varchar(48) NOT NULL PRIMARY KEY,
     `tasksCount` tinyint(4) NOT NULL DEFAULT 1,
-    `normalClothes` json NOT NULL DEFAULT '[]',
-    `playerItems` json NOT NULL DEFAULT '[]'
+    `normalClothes` json NOT NULL,
+    `playerItems` json NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
