ffb3199dd54b23901f860c67eea6763e898c0063
HR0110 <hrisiraya@abv.bg>
2025-08-10 18:53:55 +0300

    version update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ name 'HRComserv'
 author 'HRScripts Development'
 description 'A community services resource available for Ox, ESX & QBCore frameworks'
 repository 'https://github.com/HRScripts/HRComserv'
-version '1.1.6'
+version '1.1.7'
 
 shared_script '@HRLib/import.lua'
 
